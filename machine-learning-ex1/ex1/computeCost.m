f46f2e719f3e5e09ba1eff8d50c6a9abccc0fe6e
Luke Wassink <lwassink@gmail.com>
2017-08-10 18:15:50 -0400

    finish hw 1

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -6,7 +6,7 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
@@ -14,8 +14,8 @@ J = 0;
 %               You should set J to the cost.
 
 
-
-
+temp = X * theta - y;
+J = 1/(2 * m) * (temp' * temp);
 
 % =========================================================================
 
